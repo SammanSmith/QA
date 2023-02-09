a3b41dde03e8d3cebf1f644e928affe7e314b3a2
Raavan <raavan@example.com>
2023-02-09 22:13:44 +0300

    Add HW_13_8_19

---
+++ SkillFactory/Основы python/HW_13_8_19.py
@@ -0,0 +1,15 @@
+ticket = int(input("Сколько билетов хотите приобрести? >>> "))
+
+total = 0
+
+for i in range(ticket):
+    age = int(input('Введите возраст >>> '))
+    if 18 < age < 25:
+        total += 990
+    elif age > 25:
+        total += 1390
+
+if ticket > 3:
+    print(total + total * .1)
+else:
+    print(total)
